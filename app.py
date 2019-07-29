e709b602ac33049aa46e93fff5babfc97df24f88
Ryan Wilson <rwils83@gmail.com>
2019-07-29 16:07:48 -0400

    test

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
-from secrets import APiKEY
+from secrets import APIKEY
 message = Mail(
     from_email='from_email@example.com',
     to_emails='to@example.com',
